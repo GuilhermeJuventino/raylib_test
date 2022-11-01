45219c71174f738f742a10057cf6a22f24668d48
Guilherme Pereira Juventino <guilhermejuventino@gmail.com>
2022-10-31 21:25:22 -0300

    Simple Hello World window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use raylib::prelude::*;
+
+// function for drawing things to the screen
+fn draw(handle: &mut RaylibHandle, thread: &RaylibThread) {
+    let mut d = handle.begin_drawing(&thread);
+
+    d.clear_background(Color::WHITE);
+    d.draw_text("Hello world", 12, 12, 20, Color::BLACK);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (mut rl, thread) = raylib::init()
+        .size(640, 480)
+        .title("Raylib Test")
+        .build();
+
+    while !rl.window_should_close() {
+        draw(&mut rl, &thread);
+    }
 }
